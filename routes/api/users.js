d2efd261bf50637e3fb3f383a6c5285ce3b56ea7
kevin <kevinmt191919@gmail.com>
2020-07-29 00:04:02 -0700

    Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,38 @@
 const express = require('express');
 //get router tool from express
 const router = express.Router();
+const {
+  check,
+  validationResult,
+} = require('express-validator');
 
-// @route     GET api/users
-// @descrip   Test Route
+// @route     POST api/users
+// @descrip   Register user
 // @access    Public
-router.get('/',(req,res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check(
+      'email',
+      'Please include a valid email'
+    ).isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    console.log(req.body);
+    //
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    res.send('User Route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 //this file is to work with server connection
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
 const app = express();
 
 // Connect Database
 connectDB();
 
+//Init Middleware
+//allows to get data in req.body and use it
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/post', require('./routes/api/posts'))
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use(
+  '/api/profile',
+  require('./routes/api/profile')
+);
+app.use('/api/post', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
