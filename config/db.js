c53f88f50d023c14511fda15a9a46cccd3a62c61
kevin <kevinmt191919@gmail.com>
2020-07-28 18:51:13 -0700

    More Server and DB Setup

---
+++ config/db.js
@@ -0,0 +1,24 @@
+//This file is to work with db connection
+const mongoose = require('mongoose');
+//allows for global access of tools
+const config = require('config');
+//allows to connect to mongodb compass by assigning it to the login values
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,16 @@
+//this file is to work with server connection
+const express = require('express');
+const connectDB = require('./config/db')
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`)
+);
