a19d2e0515dfd466ebabc3c89ed03393bcd68996
kevin <kevinmt191919@gmail.com>
2020-08-03 23:45:41 -0700

    Posts setup

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,26 @@
 const express = require('express');
 //get router tool from express
 const router = express.Router();
+const {
+  check,
+  validationResult,
+} = require('express-validator');
+const auth = require('../../middleware/authenticateToken');
 
-// @route     GET api/posts
-// @descrip   Test Route
-// @access    Public
-router.get('/', (req, res) => res.send('Posts route'));
+// @route     POST api/posts
+// @descrip   Create a post
+// @access    Private
+router.get(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty(),
+    ],
+  ],
+  async(req, res) => {}
+);
 
 module.exports = router;
