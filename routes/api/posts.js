259d665aa65336db02c873536360de4e2c0af2a9
kevin <kevinmt191919@gmail.com>
2020-08-07 23:25:29 -0700

    Add Comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -206,4 +206,57 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
+//==============================================================================================
+
+// @route     POST api/posts/comment/:id
+// @descrip   Comment on a post
+// @access    Private
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    //If there is an error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    try {
+      //
+      const user = await User.findById(
+        req.user.id
+      ).select('-password');
+
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      //to be able to send it back as json
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+      //
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
