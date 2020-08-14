01dbb57fa55fd07312f5de9a48e0b16c34af36aa
kevin <kevinmt191919@gmail.com>
2020-08-13 22:33:37 -0700

    Additional Compoents and Bug To Fix

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,11 +2,14 @@
 import uuid from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
+
+//Actions are payloads of information that send data from your application to your store. 
+//What Dispatch does: It dispatches an ACTION. This is the only way to trigger a state change. 
 export const setAlert = (msg, alertType) => (
   dispatch
 ) => {
   const id = uuid.v4();
-
+//If you look at your react dev tools in 'Action' you can see the payload data sent out from just the function below
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id },
--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -3,11 +3,21 @@ import PropTypes from 'prop-types';
 //Connects component to Redux
 import { connect } from 'react-redux';
 
-const Alert = (props) => {
-  return <div></div>;
-};
+const Alert = ({ alerts }) =>
+  alerts !== null &&
+  alerts.length > 0 &&
+  alerts.map((alert) => (
+    <div
+      key={alert.id}
+      className={`alert alert-${alert.alertType}`}
+    >
+      {alert.msg}
+    </div>
+  ));
 
-Alert.propTypes = {};
+Alert.propTypes = {
+  alerts: PropTypes.array.isRequired,
+};
 
 //Fetch the alert state from the 'redux dev tool array' into this component
 //mapping the redux state to ----> this props so that we have access to it
