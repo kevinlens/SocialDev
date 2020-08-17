04d81bb1dbac649ccec83eabb7d619c346321aa2
kevin <kevinmt191919@gmail.com>
2020-08-16 23:53:13 -0700

    Welcome Username Section

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,14 +3,32 @@ meaing execute upon: page refresh or page load, by using '[]' it will do it only
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
+  //
   useEffect(() => {
     getCurrentProfile();
   }, []);
-
-  return <div>Dashboard</div>;
+  //usesEffect-->getCurrentProfile is running synchronously(meaning it doesn't wait) therefore 'loading' has a chance to be true(loading)
+  //'loading is true, && profile === null'
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        {/* if the 'user' exist, then show username */}
+        <i className="fas fa-user"> Welcome {user && user.name}</i>
+      </p>
+    </>
+  );
+  //
 };
 
 Dashboard.propTypes = {
