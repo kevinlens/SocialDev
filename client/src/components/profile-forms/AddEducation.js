27ffa713fc09324c21c5828d8aaa28fa836ac376
kevin <kevinmt191919@gmail.com>
2020-08-21 16:27:01 -0700

    Finished with Component for Adding Education/Experience

---
+++ client/src/components/profile-forms/AddEducation.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+//'withRouter' allows you to work with history Object like 'history.push'
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addEducation } from '../../actions/profile';
+
+const AddEducation = ({ addEducation, history }) => {
+  const [formData, setFormData] = useState({
+    school: '',
+    degree: '',
+    fieldofstudy: '',
+    from: '',
+    to: '',
+    current: false,
+    description: '',
+  });
+
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description,
+  } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addEducation(formData, history);
+  };
+  return (
+    <>
+      <h1 className="large text-primary">Add An Education</h1>
+      <p className="lead">
+        <i className="fas fa-code-branch"></i> Add any school or bootcamp that
+        you have attended
+      </p>
+      <small>* = required field</small>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Degree or Certificate"
+            value={degree}
+            onChange={(e) => onChange(e)}
+            name="degree"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* School or Bootcamp"
+            value={school}
+            onChange={(e) => onChange(e)}
+            name="school"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Field of Study"
+            value={fieldofstudy}
+            onChange={(e) => onChange(e)}
+            name="fieldofstudy"
+          />
+        </div>
+        <div className="form-group">
+          <h4>From Date</h4>
+          <input
+            type="date"
+            value={from}
+            onChange={(e) => onChange(e)}
+            name="from"
+          />
+        </div>
+        <div className="form-group">
+          <p>
+            <input
+              type="checkbox"
+              value={current}
+              checked={current}
+              //change the 'current' value to the opposite of what it is now
+              onChange={(e) => {
+                setFormData({ ...formData, current: !current });
+              }}
+              name="current"
+              value=""
+            />{' '}
+            Still Enrolled
+          </p>
+        </div>
+        <div className="form-group">
+          <h4>To Date</h4>
+          <input
+            type="date"
+            value={to}
+            onChange={(e) => onChange(e)}
+            name="to"
+            disabled={current ? 'disabled' : ''}
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            name="description"
+            cols="30"
+            rows="5"
+            value={description}
+            onChange={(e) => onChange(e)}
+            placeholder="Program Description"
+          ></textarea>
+        </div>
+        <input type="submit" className="btn btn-primary my-1" />
+        <Link className="btn btn-light my-1" to="/dashboard">
+          Go Back
+        </Link>
+      </form>
+    </>
+  );
+};
+
+AddEducation.propTypes = {
+  addEducation: PropTypes.func.isRequired,
+};
+//'withRouter' allows you to work with history Object like 'history.push'
+export default connect(null, { addEducation })(withRouter(AddEducation));
--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -1,16 +1,124 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+//'withRouter' allows you to work with history Object like 'history.push'
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addExperience } from '../../actions/profile';
 
-const AddExperience = props => {
+const AddExperience = ({ addExperience, history }) => {
+  const [formData, setFormData] = useState({
+    company: '',
+    title: '',
+    location: '',
+    from: '',
+    to: '',
+    current: false,
+    description: '',
+  });
+
+  const { company, title, location, from, to, current, description } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addExperience(formData, history);
+  };
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <h1 className="large text-primary">Add An Experience</h1>
+      <p className="lead">
+        <i className="fas fa-code-branch"></i> Add any developer/programming
+        positions that you have had in the past
+      </p>
+      <small>* = required field</small>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Job Title"
+            value={title}
+            onChange={(e) => onChange(e)}
+            name="title"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Company"
+            value={company}
+            onChange={(e) => onChange(e)}
+            name="company"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Location"
+            value={location}
+            onChange={(e) => onChange(e)}
+            name="location"
+          />
+        </div>
+        <div className="form-group">
+          <h4>From Date</h4>
+          <input
+            type="date"
+            value={from}
+            onChange={(e) => onChange(e)}
+            name="from"
+          />
+        </div>
+        <div className="form-group">
+          <p>
+            <input
+              type="checkbox"
+              value={current}
+              checked={current}
+              //change the 'current' value to the opposite of what it is now
+              onChange={(e) => {
+                setFormData({ ...formData, current: !current });
+              }}
+              name="current"
+              value=""
+            />{' '}
+            Current Job
+          </p>
+        </div>
+        <div className="form-group">
+          <h4>To Date</h4>
+          <input
+            type="date"
+            value={to}
+            onChange={(e) => onChange(e)}
+            name="to"
+            disabled={current ? 'disabled' : ''}
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            name="description"
+            cols="30"
+            rows="5"
+            value={description}
+            onChange={(e) => onChange(e)}
+            placeholder="Job Description"
+          ></textarea>
+        </div>
+        <input type="submit" className="btn btn-primary my-1" />
+        <Link className="btn btn-light my-1" to="/dashboard">
+          Go Back
+        </Link>
+      </form>
+    </>
+  );
+};
 
 AddExperience.propTypes = {
-
-}
-
-export default AddExperience
+  addExperience: PropTypes.func.isRequired,
+};
+//'withRouter' allows you to work with history Object like 'history.push'
+export default connect(null, { addExperience })(withRouter(AddExperience));
